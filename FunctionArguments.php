eb8a2ec599849165bd8160a510a4ef8ad316c675
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-03-25 09:29:14 +0530

    Function Arguments
    Function Arguments in php

---
+++ FunctionArguments.php
@@ -0,0 +1,20 @@
+<?php
+
+function familyName($fname){
+	echo $fname."<br>";
+}
+
+familyName("Gamage");
+familyName("Guruge");
+familyName("Ekanayaka");
+
+
+function name($name,$year){
+	echo "My name is ".$name.",born in ".$year."<br>";
+}
+
+name("Gayathri",1994);
+name("Tharushi",2001);
+name("Dinuki",2004);
+
+?>
